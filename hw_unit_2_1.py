705b31fbffbf7deb6a3099186d7a588f337c675b
rozovO <88148177+rozovO@users.noreply.github.com>
2021-07-29 17:34:01 +0300

    HW for unit 2

---
+++ hw_unit_2_1.py
@@ -0,0 +1,14 @@
+# Організація ввода з клавіатури input
+input_integer_value = input('Input integer value: ')
+
+# Визначення класу str
+print(type(input_integer_value))
+print(input_integer_value)
+
+# Конвертували Str to int
+input_integer_value = int(input_integer_value)
+print(type(input_integer_value))
+print(input_integer_value)
+
+
+
---
+++ hw_unit_2_2.py
@@ -0,0 +1,11 @@
+# Організація ввода з клавіатури input
+input_float_value = input('Input float value: ')
+
+# Визначення класу str
+print(type(input_float_value))
+print(input_float_value)
+
+# Конвертували Str to float
+input_float_value = float(input_float_value)
+print(type(input_float_value))
+print(input_float_value)
