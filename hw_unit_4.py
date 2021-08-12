51d8757f434581eec94cf304ccc9c25908f28366
rozovO <88148177+rozovO@users.noreply.github.com>
2021-08-12 18:20:16 +0300

    Add files via upload

---
+++ hw_unit_4.py
@@ -0,0 +1,49 @@
+# Все пункты задания отделять друг от друга строкой символов комментария. Вот так:
+#####################################################
+
+# value = 50
+# if value < 100:
+#    print("цикл", value)
+# else:
+#    new_value = 50
+
+# value_1 = 0
+# if value_1 < 100:
+#    print("цикл", value_1)
+# else:
+#    new_value_1 = 1
+
+# value_2 = False
+# if value_2 < 100:
+#    print("цикл", value_2)
+# else:
+#    new_value_2 = True
+
+
+
+# my_str = "0"
+# print(my_str[::2])
+
+# my_str = "2"
+# print(my_str[::2])
+
+# my_str = "4"
+# print(my_str[::4])
+
+# my_str = "6"
+# print(my_str[::6])
+
+# my_str = "8"
+# print(my_str[::8])
+
+# my_str = "Розов"
+# print(id(my_str))
+# my_str = "Олексій"
+# print(id(my_str))
+# print(my_str)
+
+# my_str_1 = "Олексій"
+# print(id(my_str_1))
+# my_str_1 = "Розов"
+# print(id(my_str_1))
+# print(my_str_1)
