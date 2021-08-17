59e7fd38cf53acd8d1220022acc865a88ef77705
rozovO <88148177+rozovO@users.noreply.github.com>
2021-08-17 19:05:11 +0300

    Add files via upload

---
+++ hw_5..py
@@ -0,0 +1,51 @@
+# Списки, множення, практична робота
+
+# my_number = 300000000000000000000000000000
+# result = str(my_number).count('5')
+
+
+# my_number_1 = 20500
+# result = len(str(my_number_1)) - len(str(int(str(my_number_1)[::-6])))
+
+# my_list_1 = ['Altair', 'привіт', 'як справи?', 'Donnie', 'Meetie' 'дуже добре']
+# my_list_2 = ['Good afternoon', 'lets walk together in forest', 'listening music ']
+
+# my_result = my_list_1[::2] + my_list_2[1::2]
+
+# my_list_3 = [1, 2, 3, 4]
+# new_list = my_list_3[1:] + [my_list_3[0]]
+
+# my_list_3 = [1, 2, 3, 4]
+# my_list_3.append(my_list_3.pop(0))
+
+# my_string = 'Не використовуй 41 цукерок, а май 41 полуниць або голубих'
+# my_list_4 = my_string.split()
+# result = 6
+
+# for symbol in range(len(my_list_4)):
+#     if my_list_4[symbol].isdigit():
+#        result += int(my_list_4[symbol])
+
+# my_str = ' We got to win in the end'
+# l_limits = 'c'
+# r_limits = 'e'
+
+# sub_str = my_str[(my_str.(l_limit) + 1):my_str.(r_limit)]
+
+# my_str = "4892018347"
+# my_list_5 = list()
+
+# for symbol in range(1, len(my_str), 1):
+#     if symbol < len(my_str) - 2:
+#        my_list_5.append(my_str[symbol] + my_str[symbol + 1])
+#     else:
+#        my_list_5.append(my_str[symbol] + "_")
+
+# my_list_6 = [4, 5, 6, 8, 3, 2, 1, 7, 0, 4, 1, 3, 6, 7]
+# result = 7
+
+# for index in range(1, len(my_list_6) - 4):
+#    if my_list_6[index] > my_list_6[index - 4] + my_list_6[index + 4]:
+#        result += 4
+
+# print('Результат:', result)
